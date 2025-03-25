e75637f585b5156ef89c181f010aaea0c17b57c6
Nguyen Duc <zeross0407@gmail.com>
2025-03-25 22:13:16 +0700

    pronoun assessment added

--- lib/features/lesson/presentation/bloc/lesson_bloc.dart
+++ lib/features/lesson/presentation/bloc/lesson_bloc.dart
@@ -2,17 +2,22 @@ import 'dart:async';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_linguage/features/lesson/domain/usecases/pronoun_usecase.dart';
 import 'package:go_linguage/features/lesson/domain/usecases/view_usecase.dart';
+import 'package:go_linguage/features/lesson/presentation/pages/pronoun_assessment.dart';
 import 'package:go_linguage/features/subject/data/models/api_subject_model.dart';
 part 'lesson_event.dart';
 part 'lesson_state.dart';
 
 class LessonBloc extends Bloc<ViewEvent, LessonState> {
   final LessonViewUsecase _viewUsecase;
-  LessonBloc(LessonViewUsecase viewUsecase)
+  final PronounUsecase _pronounUsecase;
+  LessonBloc(LessonViewUsecase viewUsecase, PronounUsecase pronounUsecase)
       : _viewUsecase = viewUsecase,
+        _pronounUsecase = pronounUsecase,
         super(LoadingData()) {
     on<ViewData>(_loadData);
+    on<CheckPronounEvent>(_checkPronoun);
   }
 
   Future<void> _loadData(
@@ -29,6 +34,22 @@ class LessonBloc extends Bloc<ViewEvent, LessonState> {
       },
       (successData) {
         emit.call(LoadedData(lessonModel: successData));
+      },
+    );
+  }
+
+  Future<void> _checkPronoun(
+    CheckPronounEvent event,
+    Emitter<LessonState> emit,
+  ) async {
+    emit.call(CheckPronounState(result: null, status: 0));
+    final result = await _pronounUsecase.call([event.oggPath, event.sentence]);
+    result.fold(
+      (failure) {
+        emit.call(CheckPronounState(result: null, status: -1));
+      },
+      (successData) {
+        emit.call(CheckPronounState(result: successData, status: 1));
       },
     );
   }
